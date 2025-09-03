fdbf53d43a63eb73f78e85d57da535a6d8d6ba54
gansior <gansior@exportcenter.ru>
2025-09-03 18:15:21 +0300

    add new proc with value

--- set_prog/base_class.py
+++ set_prog/base_class.py
@@ -25,15 +25,44 @@ def dictionary_analysis(str_path: str, level: int, dict_cur: dict, full_info: st
     new_level = level + 1
     for key in dict_cur:
         print(str_path + key, f", Level {new_level}")
-        full_info += str_path + key + f",Level {new_level} \n"
+        full_info += str_path + key + f"^Level {new_level} \n"
         if isinstance(dict_cur[key], dict):
             full_info = dictionary_analysis(
-                str_path + key + ",", new_level, dict_cur[key], full_info
+                str_path + key + "^", new_level, dict_cur[key], full_info
             )
         if isinstance(dict_cur[key], list):
             full_info = list_analysis(
-                str_path + key + ",", new_level, 0, dict_cur[key], full_info
+                str_path + key + "^", new_level, 0, dict_cur[key], full_info
             )
+    return full_info
+
+
+def dictionary_analysis_value(str_path: str, level: int, dict_cur: dict, full_info: str):
+    """_summary_
+
+    Args:
+        str_path (str): _description_
+        level (int): _description_
+        dict_cur (dict): _description_
+        full_info (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    new_level = level + 1
+    for key in dict_cur:
+        print(str_path + key, f", Level {new_level}")
+        full_info += str_path + key + f"^Level {new_level} \n"
+        if isinstance(dict_cur[key], dict):
+            full_info = dictionary_analysis_value(
+                str_path + key + "^", new_level, dict_cur[key], full_info
+            )
+        if isinstance(dict_cur[key], list):
+            full_info = list_analysis_value(
+                str_path + key + "^", new_level, 0, dict_cur[key], full_info
+            )
+        if isinstance(dict_cur[key], str):
+            full_info = get_value(str_path, dict_cur[key], key, level, full_info) 
     return full_info
 
 
@@ -69,6 +98,48 @@ def list_analysis(
             full_info = list_analysis(
                 str_path, level_dict, new_level_list, row, full_info
             )
+ 
+    return full_info
+
+
+def list_analysis_value(
+    str_path: str, level_dict: int, level_list: int,
+    list_cur: list, full_info: str
+):
+    """_summary_
+
+    Args:
+        str_path (str): Текущий путь в структуре данных.
+        level_dict (int): Уровень вложенности словаря.
+        level_list (int): Уровень вложенности списка.
+        list_cur (list): Текущий список для анализа.
+        full_info (str): Накопленная информация.
+
+    Returns:
+        full_info (str): Обновленная накопленная информация
+    """
+    new_level_dict = 0
+    new_level_list = 0
+    for row in list_cur:
+        new_level_list = new_level_list + 1
+        if isinstance(row, dict):
+            new_level_dict = level_dict
+            print(str_path, f", List string {new_level_list}")
+            full_info += str_path + f" List string {new_level_list}\n"
+            full_info = dictionary_analysis_value(str_path, new_level_dict,
+                                            row, full_info)
+        if isinstance(row, list):
+            print(str_path, f", List string {new_level_list}")
+            full_info += str_path + f" List string {new_level_list}\n"
+            full_info = list_analysis_value(
+                str_path, level_dict, new_level_list, row, full_info
+            )
+ 
+    return full_info
+
+
+def get_value(str_path: str, is_dict: str, key: str, level, full_info: str):
+    full_info += str_path + key + '^' + is_dict + f"^Level {level + 1} \n"
     return full_info
 
 
@@ -116,6 +187,7 @@ class Xmlworker:
         rez_str = ">".join(new_str)
         return rez_str
 
+
     @staticmethod
     def full_struct_xml(data: str):
         """_summary_
@@ -129,17 +201,45 @@ class Xmlworker:
         if isinstance(is_dict, dict):
             for key in is_dict:
                 print(key, f" Level {level}")
-                full_info += key + f",Level {level} \n"
+                full_info += key + f"^Level {level} \n"
                 if isinstance(is_dict[key], dict):
                     full_info = dictionary_analysis(
-                        key + ",", level, is_dict[key], full_info
+                        key + "^", level, is_dict[key], full_info
                     )
         if isinstance(is_dict, list):
             print('root_list', f", List string {level}")
             full_info += 'root_list,' + f" List string {level}\n"
-            full_info = dictionary_analysis(str_path,
-                                            new_level_dict,
-                                            row, full_info)
+            full_info = list_analysis('root_list', level,
+                                      level, is_dict, full_info)
+    
+        return full_info
+
+
+    @staticmethod
+    def full_struct_xml_value(data: str):
+        """_summary_
+
+        Args:
+            data (str): _description_
+        """
+        level = 0
+        is_dict = xd.parse(data)
+        full_info = ""
+        if isinstance(is_dict, dict):
+            for key in is_dict:
+                print(key, f" Level {level}")
+                full_info += key + f"^Level {level} \n"
+                if isinstance(is_dict[key], dict):
+                    full_info = dictionary_analysis_value(
+                        key + "^", level, is_dict[key], full_info
+                    )
+        if isinstance(is_dict, list):
+            print('root_list', f", List string {level}")
+            full_info += 'root_list,' + f" List string {level}\n"
+            full_info = list_analysis_value('root_list', level,
+                                      level, is_dict, full_info)
+        if isinstance(is_dict, str):
+            full_info = get_value('root^', is_dict, 'root', level, full_info)
         return full_info
 
 
