9ba98a2ea202d6caabee456e44078882cfe68ae1
gansior <gansior@exportcenter.ru>
2025-09-03 14:19:32 +0300

    add new procedures full_struct_xml

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ Starting 2025/09/03
 Ending 2025//
 
 '''
+import sys
+import os
 
-from set_prog.base_class import XML_WORKER
+
+current_directory = os.getcwd()
+print(current_directory)
+sys.path.append(current_directory)
 
 
-xml_worker = XML_WORKER()
+from set_prog.base_class import Xmlworker
 
-with open('/home/gansior/MyProject/xml_worker/test_datasets/1020700588688.xml', 'r', encoding='utf-8') as i_f:
+xml_worker = Xmlworker()
+# name_file = '/home/gansior/MyProject/xml_worker/test_datasets/1020700588688.xml'
+name_file = '/home/gansior/MyProject/xml_worker/test_datasets/push_mes_egrul.xml'
+# name_file = '/home/gansior/MyProject/xml_worker/test_datasets/schemas_egrul/fns-egrulotn-types.xsd'
+# name_file = '/home/gansior/MyProject/xml_worker/test_datasets/schemas_egrul/schemas.xsd'
+with open(name_file, 'r', encoding='utf-8') as i_f:
     data = i_f.read()
-    data_clear = xml_worker(data)
+    data_clear = xml_worker.xml_clear(data)
+    f_str = xml_worker.full_struct_xml(data_clear)
+    name_rezult = name_file.split('/')[-1].split('.')[0]
+    with open(f'rezult_{name_rezult}.csv', 'w', encoding='utf-8') as r_f:
+        num_str = 0
+        for row in f_str.split('\n'):
+            r_f.write(str(num_str) + ',' + row + '\n')
+            num_str += 1
--- set_prog/base_class.py
+++ set_prog/base_class.py
@@ -7,8 +7,72 @@ Ending 2025//
 
 """
 
+import xmltodict as xd
 
-class XML_WORKER:
+
+def dictionary_analysis(str_path: str, level: int, dict_cur: dict, full_info: str):
+    """_summary_
+
+    Args:
+        str_path (str): _description_
+        level (int): _description_
+        dict_cur (dict): _description_
+        full_info (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    new_level = level + 1
+    for key in dict_cur:
+        print(str_path + key, f", Level {new_level}")
+        full_info += str_path + key + f",Level {new_level} \n"
+        if isinstance(dict_cur[key], dict):
+            full_info = dictionary_analysis(
+                str_path + key + ",", new_level, dict_cur[key], full_info
+            )
+        if isinstance(dict_cur[key], list):
+            full_info = list_analysis(
+                str_path + key + ",", new_level, 0, dict_cur[key], full_info
+            )
+    return full_info
+
+
+def list_analysis(
+    str_path: str, level_dict: int, level_list: int,
+    list_cur: list, full_info: str
+):
+    """_summary_
+
+    Args:
+        str_path (str): Текущий путь в структуре данных.
+        level_dict (int): Уровень вложенности словаря.
+        level_list (int): Уровень вложенности списка.
+        list_cur (list): Текущий список для анализа.
+        full_info (str): Накопленная информация.
+
+    Returns:
+        full_info (str): Обновленная накопленная информация
+    """
+    new_level_dict = 0
+    new_level_list = 0
+    for row in list_cur:
+        new_level_list = new_level_list + 1
+        if isinstance(row, dict):
+            new_level_dict = level_dict
+            print(str_path, f", List string {new_level_list}")
+            full_info += str_path + f" List string {new_level_list}\n"
+            full_info = dictionary_analysis(str_path, new_level_dict,
+                                            row, full_info)
+        if isinstance(row, list):
+            print(str_path, f", List string {new_level_list}")
+            full_info += str_path + f" List string {new_level_list}\n"
+            full_info = list_analysis(
+                str_path, level_dict, new_level_list, row, full_info
+            )
+    return full_info
+
+
+class Xmlworker:
     """Обработчик XML данных"""
 
     @staticmethod
@@ -16,7 +80,8 @@ class XML_WORKER:
         """Очистка xml от артефактов двойных кавычек
 
         Args:
-            ddd (str): XML строка, в которой заменяются дублирующиеся двойные кавычки на &quot;
+            ddd (str): XML строка, в которой заменяются дублирующиеся
+                       двойные кавычки на &quot;
 
         Returns:
             str: Очищенная XML строка
@@ -31,13 +96,14 @@ class XML_WORKER:
                 for fr2_str in ser1_list:
                     if fr2_str.count('"') > 2:
                         com_str_error += 1
-                        fr3_str = fr2_str[fr2_str.find('"') + 1 :]
-                        fr4_str = fr3_str[: fr3_str.rfind('"')].replace('"', "&quot;")
+                        fr3_str = fr2_str[fr2_str.find('"') + 1:]
+                        fr4_str = fr3_str[: fr3_str.rfind('"')].replace('"',
+                                                                        "&quot;")
                         serv_str = (
                             "="
                             + fr2_str[: fr2_str.find('"') + 2]
                             + fr4_str
-                            + fr2_str[fr2_str.rfind('"') :]
+                            + fr2_str[fr2_str.rfind('"'):]
                         )
                         new_str2.append(serv_str)
                     elif 0 < fr2_str.count('"') <= 2:
@@ -49,3 +115,30 @@ class XML_WORKER:
             new_str.append("".join(new_str2))
         rez_str = ">".join(new_str)
         return rez_str
+
+    @staticmethod
+    def full_struct_xml(data: str):
+        """_summary_
+
+        Args:
+            data (str): _description_
+        """
+        level = 0
+        is_dict = xd.parse(data)
+        full_info = ""
+        if isinstance(is_dict, dict):
+            for key in is_dict:
+                print(key, f" Level {level}")
+                full_info += key + f",Level {level} \n"
+                if isinstance(is_dict[key], dict):
+                    full_info = dictionary_analysis(
+                        key + ",", level, is_dict[key], full_info
+                    )
+        if isinstance(is_dict, list):
+            print('root_list', f", List string {level}")
+            full_info += 'root_list,' + f" List string {level}\n"
+            full_info = dictionary_analysis(str_path,
+                                            new_level_dict,
+                                            row, full_info)
+        return full_info
+
